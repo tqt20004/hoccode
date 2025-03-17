e7ebae6213b125c8a3c7d0fc0ec12f0bf5d76e59
thang <thangtu0440@gmail.com>
2025-03-17 16:37:20 +0700

    hi

---
+++ hoccode/hoccode/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hoccode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           /////  input va output Ten + nam sinh
+            string name;
+            Console.WriteLine("input name");
+            name = Console.ReadLine();
+            Console.WriteLine("input the year that you was born");
+            int bornYear = int.Parse(Console.ReadLine());
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Your name is:"+ name);
+            Console.WriteLine("Your born year:"+ bornYear); 
+        }
+    }
+}
